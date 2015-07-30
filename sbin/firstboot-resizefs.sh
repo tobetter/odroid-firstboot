1b52b9eec83ac417199d2c6258b3db482a3b53da
Dongjin Kim <tobetter@gmail.com>
2015-07-31 02:10:35 +0900

    Add resizefs script for firstboot
    /sbin/firstboot-resizefs.sh will be called on first boot and it will
    resize the partition specified.
    
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

---
+++ sbin/firstboot-resizefs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+[ -z "$1" ] && echo "Disk to resize the partition is not selected!" && exit 1
+[ -z "$2" ] && echo "Please specify the partition index!" && exit 1
+
+disk="$1"
+partnum="$2"
+partition="${disk}p${partnum}"
+
+start=$(parted ${disk} -ms unit s p | grep "^${partnum}" | cut -f 2 -d:)
+
+echo "Extending the partition ${partition}..."
+
+fdisk ${disk} <<__EOF > /dev/null
+p
+d
+${partnum}
+n
+p
+${partnum}
+${start}
+
+w
+__EOF
+
+echo "Resizing the partition..."
+
+partprobe
+resize2fs ${partition}
+fdisk -l ${disk}
