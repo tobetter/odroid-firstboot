12cc1c905815f4f962dae680de92d7c82993a357
Dongjin Kim <tobetter@gmail.com>
2016-06-30 02:20:13 -0400

    refactoring /sbin/odroid-firstboot.sh :)
    Signed-off-by: Dongjin Kim <tobetter@gmail.com>

--- sbin/odroid-firstboot.sh
+++ sbin/odroid-firstboot.sh
@@ -9,43 +9,35 @@ reboot=false;
 
 footprint="/.firstboot"
 
-rootfs_growup() {
+
+on_firstboot() {
+	#
+	# Extend the root partition as much as available space in boot disk.
+	#
         start=$(parted ${device} -ms unit s p | grep "^${partnum}" | cut -f 2 -d:)
-
-        fdisk ${device} <<__EOF > /dev/null
-p
-d
-${partnum}
-n
-p
-${partnum}
-${start%s}
-
-w
-__EOF
+	echo -e "o\nd\n${partnum}\nn\np\n${partnum}\n${start%s}\n\nw" | fdisk ${device}
         echo "I: the partition table for ${partition} is changed..."
 
+	#
+	# Display reboot message
+	#
 	ttydev=`tty | sed -e "s:/dev/::"`
-	if [ "${ttydev}" != "not a tty" ]; then
-		dialog --title "Reboot" \
-			--pause "The partition size of ${partition} is changed \
-and will be resized on next boot. Your system will restart in 5 seconds." 11 40 5
-	fi
+	[ "${ttydev}" != "not a tty" ] && dialog --title "Firstboot" --pause "The partition size of ${partition} is changed and will be resized on next boot. Your system will restart in 5 seconds." 11 40 5
 	if [ "$?" = "0" ]; then
 		mount -o remount,rw /
-		echo "I: root file system is remounted!"
 		echo ${partition} > ${footprint}
+		echo "I: ${footprint} is created"
 		reboot="true"
-		echo "I: /.firstboot is created, the system is going to reboot..."
 	fi
+
+	[ "$reboot" = "true" ] && reboot || exit 0
 }
 
-echo ${footprint}
-if [ ! -f ${footprint} ]; then
-        rootfs_growup
-	[ "$reboot" = "true" ] && reboot || exit 0
-fi
+[ ! -f ${footprint} ] && on_firstboot
 
+###############################################################################
+# Start 2nd stage of firstboot & resizing the root file system
+#
 echo "I: the partition, ${partition}, is being resized..."
 resize2fs ${partition}
 fdisk -l ${device}
@@ -72,6 +64,9 @@ dpkg-query --status ${hwpack} > /dev/null 2>&1
 echo "I: cleaning unnecessary package(s)..."
 dpkg --purge ${packages}
 
+#
+# Remove footprint to start Firstboot
+#
 rm -f ${footprint}
 
 echo "I: The 'firstboot' is finished, your system is initiated completely"
